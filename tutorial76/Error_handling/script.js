97a40b49af6bec70d822f77edcfe1d37d380a2b9
Bharat8989 <kadamb208@gmail.com>
2024-03-04 18:22:55 +0530

    error handling files add

---
+++ tutorial76/Error_handling/script.js
@@ -0,0 +1,8 @@
+// let a= prompt("enter the value of a:");
+// let b = prompt("enter the value of b:");
+let sum=parseInt(23)+parseInt(45);
+let c=prompt("enter your name");
+let d=prompt("enter your surname:");
+let sum1=String(c)+ String(d);
+console.log("the sum is ", sum);
+console.log("the sum1 is :f",sum1);
