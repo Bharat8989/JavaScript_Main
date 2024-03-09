80b356a5b33354e1074a43a8892ce90d2d36da34
Bharat8989 <kadamb208@gmail.com>
2024-03-09 09:30:32 +0530

    adding files for modules

---
+++ tutorial86/Modules_/mymodule.js
@@ -0,0 +1,11 @@
+export const a=1;// named export
+export const b=3;
+export const c=2;
+export const d=4;
+
+const obj={
+    x:5,
+    y:6
+
+}
+export default obj; //defafult export
--- tutorial86/Modules_/script.js
+++ tutorial86/Modules_/script.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
@@ -9,3 +10,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+import {a,b,c} from "./mymodule.js"
+console.log(a);
+import  bharat from "./mymodule.js"
+console.log(bharat);
